ce7a60830232a589de46543a31046308e9e490b7
Ole Kirkeby <ole.kirkeby@m3guitar.com>
2017-07-05 22:47:02 +0300

    Enabled csv import of ods source file

---
+++ importcsv.m
@@ -0,0 +1,3 @@
+function x = importcsv(file)
+
+x = dlmread(file,',',1,0);
--- m3print.m
+++ m3print.m
@@ -1,38 +1,36 @@
-function m3print(x)
-
-#x=[15 16 18 15 16 18 23; 1.1 1.3 1.45 3.1 3.3 3.4 3.45; 2 1.5 4.5 2 1 0.5 4.5]
+function m3print(x, start, end)
 
 MARKERSIZE = 10;
+MINSTAFFLINE =  8;
+MAXSTAFFLINE = 44;
+
 [R,C] = size(x);
 
-notes      = x(1,:);
-onsets_raw = x(2,:);
-durations  = x(3,:);
+onsets    = x(:,1) + (x(:,2)-1)/4; #in bars
+durations = x(:,3);                #in quaternotes
+notes     = x(:,4);                #relative to low E = 8
+
+# Plot limits
+x0 = start;
+x1 = min(ceil(max(onsets + durations/4)),end);
+y0 = MINSTAFFLINE;
+y1 = max([ceil(max(notes)),MAXSTAFFLINE]);
 
 n0 = find(mod(notes,4) == 0);
 n1 = find(mod(notes,4) == 1);
 n2 = find(mod(notes,4) == 2);
 n3 = find(mod(notes,4) == 3);
 
-onsets_bar  = floor(onsets_raw);
-onsets_beat = 10 * (onsets_raw - onsets_bar - 0.1) / 4;
-onsets = onsets_bar + onsets_beat;
-
-x0 = 1;#floor(min(onsets_raw));
-x1 = ceil(max(onsets_raw + durations/4));
-y0 = 8;#floor(min(notes));
-y1 = max([ceil(max(notes)),44]);
-
 # Note onsets
 plot(onsets(n0), notes(n0),'ok','markersize', MARKERSIZE, 'markerfacecolor','k');
 grid off;
 hold on;
-plot(onsets(n1), notes(n1),'vg','markersize', MARKERSIZE, 'markerfacecolor','g');
+plot(onsets(n1), notes(n1),'vg','markersize', MARKERSIZE  , 'markerfacecolor','g');
 plot(onsets(n2), notes(n2),'sk','markersize', MARKERSIZE-2, 'markerfacecolor','k');
-plot(onsets(n3), notes(n3),'^r','markersize', MARKERSIZE, 'markerfacecolor','r');
+plot(onsets(n3), notes(n3),'^r','markersize', MARKERSIZE  , 'markerfacecolor','r');
 
 # Duration lines
-for n = 1:C
+for n = 1:R
     if (durations(n) > 0)
         plot(onsets(n)+[0,durations(n)/4],[notes(n),notes(n)],'-k','linewidth',1.5);
     endif
@@ -54,9 +52,8 @@ for n = x0:0.25:x1
 endfor
 
 # Horizontal lines, E
-for n = 8:12:44
+for n = MINSTAFFLINE:12:MAXSTAFFLINE
     plot([x0,x1],[n,n],'-k');
-#    plot([x0,x1],[n+0.5,n+0.5],'-k');
 endfor
 
 hold off;
